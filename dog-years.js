be413461dd4ac8b2536a5684bdef696e614ec019
oliver-gomes <ogomesphoto@gmail.com>
2018-07-19 13:29:16 -0400

    Converting myAge to age in dog years

---
+++ dog-years.js
@@ -0,0 +1,18 @@
+// assigning my age
+const myAge = 22;
+
+// early years value
+let earlyYears = 2;
+
+earlyYears *= 10.5;
+
+let laterYears = myAge - 2;
+
+laterYears *= 4;
+
+// adding to get age in dog years
+let myAgeInDogYears = earlyYears + laterYears;
+
+let myName = 'Oliver'.toLowerCase();
+
+console.log(`My name is ${myName}. I am ${myAgeInDogYears} years old in dog years`);
