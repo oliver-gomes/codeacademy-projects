50c7d56d65604e3b1ad328272ac3df3eef069412
oliver-gomes <ogomesphoto@gmail.com>
2018-07-21 00:28:13 -0400

    Simple for loop

---
+++ for-loop.js
@@ -0,0 +1,5 @@
+const vacationSpots =['Paris', 'Peru', 'Switzerland'];
+
+for(let vacationSpotIndex = 0; vacationSpotIndex < vacationSpots.length; vacationSpotIndex++ ){
+  console.log('I would love to visit ' + vacationSpots[vacationSpotIndex]);
+}
